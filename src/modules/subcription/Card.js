23ba61c9298488ff0c024cb8999485d3e0c1b9dd
fenikothadiya <fenikothadiya.trinityinfosystem@gmail.com>
2024-02-12 17:40:49 +0530

    company page

--- src/modules/subcription/Card.js
+++ src/modules/subcription/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles/card.module.scss';
-
+import  {Link} from 'react-router-dom'
 const Card = (props) => {
   return (
     <div className={styles.cardcontainer}>
@@ -14,7 +14,7 @@ const Card = (props) => {
          <p className={styles.bulletPoint}>Key Features:</p>
           <li> <span className={styles.bulletPoint}>{props.bulletPoint}</span></li>
         </div>
-        <button className={styles.startFreeButton}><div className={styles.btntext}>Start for Free</div></button>
+        <button className={styles.startFreeButton}><Link to='/subscription/company' className={styles.btntext}>Start for Free</Link></button>
         <p className={styles.trialText}>30 day free trial to start</p>
       </div>
     </div>
