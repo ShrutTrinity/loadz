e51dcad68af9e7f7fa2c509d178d364cadeabf1f
fenikothadiya <fenikothadiya.trinityinfosystem@gmail.com>
2024-02-13 15:05:06 +0530

    complate validation in login and forgotpass

--- src/modules/forgetpassword/ForgetPass.js
+++ src/modules/forgetpassword/ForgetPass.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './styles/forgetpassword.module.scss'
-import Card from '../../card/Card';
+import styles from './styles/forgetpassword.module.scss';
 import EmailIcon from '@mui/icons-material/Email';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
+import Card from '../../card/Card';
 
 
 const ForgetPass = () => {
@@ -13,27 +13,43 @@ const ForgetPass = () => {
   return (
     <>
       <div className={styles.container}>
-        <Card>
-          <h6 className={styles.recover}>Recover Your Password</h6>
-          <div className={styles.multiInput}>
-            <TextField required
-              sx={{ marginLeft: "0.7rem", borderBottom: '0' }}
-              InputProps={{ disableUnderline: true }}
-              id="standard-basic" label="Email" variant="standard" />
-            <div className={styles.icon}>
-              <EmailIcon sx={{ fontSize: 20 }} />
+        
+          <Card>
+            <h6 className={styles.recover}>Recover Your Password</h6>
+            <form >
+              <div className={styles.multiInput}>
+                <TextField
+                  sx={{ marginLeft: "0.7rem", borderBottom: '0', zIndex: '2' }}
+                  type='email'
+                  InputProps={{ disableUnderline: true }}
+                  id="email"
+                  name="email"
+                  label="Email &nbsp;"
+                  required
+                  variant="standard"
+                
+                />
+
+                <div className={styles.icon}>
+                  <EmailIcon sx={{ fontSize: 20 }} />
+                </div>
+              </div>
+
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ textTransform: 'none', backgroundColor: "black", borderRadius: 18, width: "14rem", ":hover": { backgroundColor: "black" } }}
+              >
+                <RouterLink to="#" className={styles.resetlink}>Send reset link</RouterLink>
+              </Button>
+            </form>
+            <div>
+              <div className={styles.link}>
+                <RouterLink to="/" >{'Go Back To Login'}</RouterLink>
+              </div>
             </div>
+          </Card>
           </div>
-          <Button variant="contained" sx={{ textTransform: 'none', backgroundColor: "black", borderRadius: 18, width: "14rem", ":hover": { backgroundColor: "black" } }}>
-            <RouterLink to="#" className={styles.resetlink}>Send reset link</RouterLink>
-          </Button>
-          <div>
-            <div className={styles.link}>
-              <RouterLink to="/" >{'Go Back To Login'}</RouterLink>
-            </div>
-          </div>
-        </Card>
-      </div>
     </>
   );
 }
--- src/modules/signIn/LoginPage.js
+++ src/modules/signIn/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import styles from './styles/loginPage.module.scss';
@@ -12,7 +12,7 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Link as RouterLink } from 'react-router-dom';
 import Card from '../../card/Card';
 
-const versions = "V1.0.0/ v1.0.1"
+
 const LoginPage = () => {
     const formik = useFormik({
         initialValues: {
@@ -28,89 +28,112 @@ const LoginPage = () => {
         },
     });
 
-  const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = React.useState(false);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-  return (
-    <>
-      <div className={styles.root}>
-        <Card>
-          <h6 className={styles.heading}>Login to your account</h6>
-          <form>
-            <div className={styles.formControl1}>
-              <TextField id="standard-basic" required label="Email"
-                variant="standard"
-                InputProps={{
-                  disableUnderline: true,
-                  endAdornment: (<div className={styles.icon}><EmailIcon /></div>)
-                }} />
+    return (
+        <>
+            <div className={styles.root}>
+                <Card>
+                            <h6 className={styles.heading}>Login to your account</h6>
+                            <form onSubmit={formik.handleSubmit}>
+                                <div className={styles.formControl1}>
+                                    <TextField
+                                        id='email'
+                                        name='email'
+                                        type='email'
+                                        label='Email'
+                                        variant='standard'
+                                        required
+                                        InputProps={{
+                                            disableUnderline: true,
+                                            endAdornment: (
+                                                <div className={styles.icon}>
+                                                    <EmailIcon />
+                                                </div>
+                                            ),
+                                        }}
+                                    />
+                                </div>
+                                <div className={styles.formControl11}>
+                                    <TextField
+                                        id='password'
+                                        name='password'
+                                        label='Password'
+                                        variant='standard'
+                                        required
+                                        type={showPassword ? 'text' : 'password'}
+                                        InputProps={{
+                                            disableUnderline: true,
+                                            endAdornment: (
+                                                <div className={styles.icon} onClick={togglePasswordVisibility}>
+                                                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                                </div>
+                                            ),
+                                        }}
+                                        {...formik.getFieldProps('password')}
+                                    />
+
+                                </div>
+                                <div className={styles.errorText} style={{ color: 'red',marginBottom:'16px',fontSize:'12px' }}>
+                                        {formik.touched.password && formik.errors.password}
+                                    </div>
+                                <div className={styles.bottomform}>
+                                    <div className={styles.checkbox}>
+                                        <Checkbox
+                                            {...formik.getFieldProps('rememberMe')}
+                                            sx={{
+                                                marginLeft: '-0.5rem',
+                                                color: grey[900],
+                                                '&.Mui-checked': {
+                                                    color: grey[900],
+                                                },
+                                            }}
+                                        />
+                                        <div className={styles.label1}>Remember Me</div>
+                                    </div>
+                                    <RouterLink className={styles.a} to='/forgetPassword'>
+                                        Forgot Password?
+                                    </RouterLink>
+                                </div>
+                                <Button
+                                    type='submit'
+                                    variant='contained'
+                                    sx={{ backgroundColor: 'black', ':hover': { backgroundColor: 'black' }, borderRadius: '1.125rem', width: '16rem', padding: '1.2', marginTop: '1rem', margin: '0 0rem' }}
+                                >
+                                    Login
+                                </Button>
+                            </form>
+
+                            <div className={styles.bottomdetail}>
+                                <span>Don't have an account?</span>
+                                <RouterLink className={styles.a2} to='/subscription'>
+                                    Create an account
+                                </RouterLink>
+                            </div>
+                            <RouterLink className={styles.a2} style={{ marginTop: '0.5rem', marginBottom: '1.5rem' }} to='/customerportal/login'>
+                                Want to Pay Hauling Invoices?
+                            </RouterLink>
+                       </Card>
+                <span style={{ color: 'white' }}>V1.0.0/ v1.0.1</span>
             </div>
-            <div className={styles.formControl1}>
-              <TextField id="standard-basic" required label="Password"
-                variant="standard"
-                type={showPassword ? 'text' : 'password'}
-                InputProps={{
-                  disableUnderline: true,
-                  endAdornment: (
-                    <div className={styles.icon} onClick={togglePasswordVisibility}>
-                      {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            <div className={styles.toolbar}>
+                <div className={styles.toolbarDetail}>
+                    <div className={styles.toolbarDetail}>
+                        <RouterLink to='/terms' className={styles.bottomLink}>
+                            Terms and conditions
+                        </RouterLink>
+                        <RouterLink className={styles.bottomLink} to='/privacy'>
+                            Privacy Policy
+                        </RouterLink>
                     </div>
-                  ),
-                }}
-              />
+                </div>
             </div>
-            <div className={styles.bottomform}>
-              <div className={styles.checkbox}>
-                <Checkbox sx={{
-                  marginLeft: '-0.5rem',
-                  color: grey[900],
-                  '&.Mui-checked': {
-                    color: grey[900],
-                  },
-                }} />
-                <div className={styles.label1}>Remember Me</div>
-              </div>
-              <RouterLink className={styles.a} to='/forgetPass'>Forgot Password?</RouterLink>
-            </div>
-            <Button variant="contained"
-              sx={{
-                backgroundColor: "black",
-                ":hover": { backgroundColor: "black" },
-                borderRadius: "1.125rem",
-                width: "16rem",
-                padding: "1.2",
-                margin: "0 0rem",
-                fontWeight: '300',
-                textTransform: 'capitalize'
-              }}>
-              Login
-            </Button>
-          </form>
+        </>
+    );
+};
 
-          <div className={styles.bottomdetail}>
-            <span style={{ fontSize: '13px' }}>Don't have an account?</span>
-            <RouterLink className={styles.a2} to='/subscription'>
-              Create an account
-            </RouterLink>
-          </div>
-          <RouterLink className={styles.a2}
-            style={{ marginTop: '0.5rem', marginBottom: '1.5rem' }}
-            to='/customerportal/login'>
-            Want to Pay Hauling Invoices?
-          </RouterLink>
-        </Card>
-        <span style={{ color: 'white' }}>{versions}</span>
-      </div>
-      <div className={styles.toolbar}>
-        <div className={styles.toolbarDetail}>
-          <RouterLink to='/terms' className={styles.bottomLink}>Terms and conditions</RouterLink>
-          <RouterLink className={styles.bottomLink} to='/privacy'>Privacy Policy</RouterLink>
-        </div>
-      </div>
-    </>
-  )
-}
-export default LoginPage
+export default LoginPage;
