6086ec2a41ab8acc32549b0c344d8d083fad0815
fenikothadiya <fenikothadiya.trinityinfosystem@gmail.com>
2024-02-16 17:10:39 +0530

    change from feni side

--- src/modules/newAccount/CreateAccount.js
+++ src/modules/newAccount/CreateAccount.js
@@ -3,8 +3,6 @@ import CustomStepper from '../../components/stepper/CustomStepper'
 import BoxComponent from '../../components/formBox/Box';
 import Logo from '../../images/loadzlogo.png';
 import styles from './styles/new.module.scss';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import EmailIcon from '@mui/icons-material/Email';
 import { grey } from '@mui/material/colors';
 import Checkbox from '@mui/material/Checkbox';
@@ -14,63 +12,41 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Link as RouterLink } from 'react-router-dom';
 import Send from '../../components/uploadFile/Send';
-import FilePopUp from '../../components/uploadPopUp';
-
-
-const CustomTextField = ({ label, placeholder, type, children4 }) => {
-  return (
-    <FormControl
-      sx={{
-        width: '100%'
-      }}
-      variant="standard">
-      <label className={styles.formLabel1}>{label}<span className={styles.requiredAll}>*</span></label>
-      <TextField
-        sx={{
-          width: '100%',
-          marginBottom: '16px'
-        }}
-        type={type}
-        placeholder={placeholder} required id="fullWidth"
-        InputProps={{
-          endAdornment: (children4)
-        }}
-      />
-    </FormControl>
-  )
-}
+import useVideoPopup from '../../hooks/useVideopopup'
+import Video from '../../components/videotag'
+import CustomTextField from '../../components/textfield/CustomeTextfield';
 
 const CreateAccount = () => {
 
   const [showPassword, setShowPassword] = useState(false);
-  const [showPopUp, setShowPopUp] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
-  const handleClickOpen = () => {
-    setShowPopUp(!showPopUp)
+ 
+  const { isPopupOpen, handleToggle }  = useVideoPopup();
+  const handleClick = () => {
+    handleToggle();
   };
-  const handleClosePop = () => {
-    setShowPopUp(false);
-  }
 
-  const handleImageSelect = (imageUrl) => {
-    setSelectedImage(imageUrl);
-
-  };
 
   return (
     <>
-      {showPopUp && <FilePopUp imageUrlforPopUp={selectedImage || persionImage} onSelect={handleImageSelect} onClose={handleClosePop}/>}
+       {isPopupOpen && <Video
+        className={styles.popup}
+        setIsOpen={handleToggle}  
+        srcLink='https://player.vimeo.com/video/785867491'
+        title='COMPANY SETUP' 
+        discription='Create your companys administrators account'
+        
+      />}
       <CustomStepper currentstep='1' />
-      <BoxComponent className={styles.mainContainer}
+      <BoxComponent className={styles.mainContainer}  handleClick={handleClick}
+
         children1={
           <img src={Logo} className={styles.loadzLogo} alt='logo' />
         }
-        children2={''}
+  
         children3={
           <form>
             <div className={styles.mainDiv}>
@@ -167,11 +143,16 @@ const CreateAccount = () => {
                   <a href='/'>Login</a>
                 </div>
               </div>
-              <Send children={selectedImage ? <img src={selectedImage} alt='Selecte persion' onClick={handleClickOpen} /> : <img src={persionImage} alt='Default Person logo' onClick={handleClickOpen} />} />
+              <Send
+                children={
+                  <img src={persionImage} alt='Upload person logo' />
+                }
+              />
             </div>
           </form>
         }
       />
+
     </>
   )
 }
