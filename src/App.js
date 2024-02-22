50b3d5181f9d17723e80526ca16d923528ed2806
ShrutTrinity <shrut.trinityinfosystem@gmail.com>
2024-02-22 14:06:32 +0530

    same thing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Welcome from './modules/company/Welcome';
 import CreateAccount from './modules/newAccount/CreateAccount';
 import SetupOfCompany from './modules/companySetup/SetupOfCompany';
 
-
 const App = () => {
  
   return (
@@ -28,13 +27,10 @@ const App = () => {
           <Route exact path='/subscription/company/0' element={<Welcome />} />
           <Route exact path='/subscription/company/1' element={<CreateAccount />} />
           <Route exact path='/subscription/company/2' element={<SetupOfCompany />} />
-     
-
         </Routes>
       </Router>
     </>
-  )
-    
+  )   
 }
 
 export default App
