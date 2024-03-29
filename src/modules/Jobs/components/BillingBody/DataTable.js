f36a3f4e5d4014d2cf019f86bbc415685f0c30bf
fenikothadiya <fenikothadiya.trinityinfosystem@gmail.com>
2024-03-29 16:15:09 +0530

    main

--- src/modules/Jobs/components/BillingBody/DataTable.js
+++ src/modules/Jobs/components/BillingBody/DataTable.js
@@ -11,26 +11,26 @@ const DataTable = ({ formData, handleDelete, handleRowClick }) => {
 
   return (
     <>
-      {formData && formData.length > 0 && ( 
-        <Table sx={{ maxWidth: 550, borderTop:1 }} aria-label="simple table">
+           {formData && formData.length > 0 && ( 
+        <Table sx={{ maxWidth: 850, borderCollapse: 'collapse' }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell sx={{ fontSize: '17px' }}>Name</TableCell>
-              <TableCell sx={{ fontSize: '17px' }} align="center">Type</TableCell>
-              <TableCell sx={{ fontSize: '17px' }} align="center">Special Rate</TableCell>
-              <TableCell sx={{ fontSize: '17px' }} align="center"></TableCell>
-              <TableCell sx={{ fontSize: '17px' }} align="center"></TableCell>
+              <TableCell sx={{ fontSize: '19px', fontWeight:400,borderBottom: '1px solid #000' }}>Name</TableCell>
+              <TableCell sx={{ fontSize: '19px',fontWeight:400, borderBottom: '1px solid #000' }} align="center">Type</TableCell>
+              <TableCell sx={{ fontSize: '19px', fontWeight:400,borderBottom: '1px solid #000' }} align="center">Special Rate</TableCell>
+              <TableCell sx={{ fontSize: '19px',fontWeight:400, borderBottom: '1px solid #000' }} align="center"></TableCell>
+              <TableCell sx={{ fontSize: '19px',fontWeight:400, borderBottom: '1px solid #000' }} align="center"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {
               formData.map((item, index) => (
-                <TableRow key={index} sx={{ 'td': { border: 1, borderRight:'unset',borderTop:1}, 'th':{border:1} }}>
-                  <TableCell sx={{ fontSize: '17px' }} align="center">{item.name === '' ? 'abc' : item.name}</TableCell>
-                  <TableCell sx={{ fontSize: '17px' }} align="center">{item.persionTypeValue}</TableCell>
-                  <TableCell sx={{ fontSize: '17px' }} align="center">${item.rate}</TableCell>
-                  <TableCell sx={{ fontSize: '17px' }} align="center"><DeleteIcon onClick={() => handleDelete(index)} /></TableCell>
-                  <TableCell sx={{ fontSize: '17px' }} align="center" onClick={() => handleRowClick(index)}> {/* Added index argument */}
+                <TableRow key={index}>
+                  <TableCell sx={{ fontSize: '19px', borderBottom: '1px solid #000',borderRight: '1px solid #000' }} align="center">{item.name === '' ? 'abc' : item.name}</TableCell>
+                  <TableCell sx={{ fontSize: '19px', borderBottom: '1px solid #000',borderRight: '1px solid #000' }} align="center">{item.persionTypeValue}</TableCell>
+                  <TableCell sx={{ fontSize: '19px', borderBottom: '1px solid #000',borderRight: '1px solid #000' }} align="center">${item.rate}</TableCell>
+                  <TableCell sx={{ fontSize: '19px', borderBottom: '1px solid #000',borderRight: '1px solid #000' }} align="center"><DeleteIcon onClick={() => handleDelete(index)} /></TableCell>
+                  <TableCell sx={{ fontSize: '19px', borderBottom: '1px solid #000' }} align="center" onClick={() => handleRowClick(index)}> {/* Added index argument */}
                     <EditIcon />
                   </TableCell>
                 </TableRow>
