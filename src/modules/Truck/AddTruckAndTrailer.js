23edc9a91b4fca482ad0d7f2b8299f03a0954e34
ShrutTrinity <shrut.trinityinfosystem@gmail.com>
2024-04-13 11:29:18 +0530

    add some ui

--- src/modules/Truck/AddTruckAndTrailer.js
+++ src/modules/Truck/AddTruckAndTrailer.js
@@ -89,10 +89,10 @@ const AddTruckAndTrailer = () => {
             <div className={styles.cards}>
               {Data.map((item, index) => (
                 <div className={styles.card} key={index}>
-                  <h3 className={styles.cardHeading}>Truck No: 1</h3>
+                  <h3 className={styles.cardHeading}>Truck No: {index +1}</h3>
                   <div className={styles.cardContent}>
                     <div className={styles.cardImage}>
-                      hii
+                      Image
                     </div>
                     <div className={styles.cardData}>
                       <div className={styles.cardValue}>
