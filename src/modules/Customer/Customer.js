fbb2f73de7b503efc2b3d9698e1eaeb098feb292
ShrutTrinity <shrut.trinityinfosystem@gmail.com>
2024-05-27 16:46:13 +0530

    add cutomer drawer

--- src/modules/Customer/Customer.js
+++ src/modules/Customer/Customer.js
@@ -17,37 +17,26 @@ import ApplyPayment from './Components/ApplyPayment/ApplyPayment';
 import CreateCustomer from './Components/CreateCustomer/CreateCustomerModel';
 import SettlementReportDialog from './Components/SettelemntReport/SettlementReportDialog';
 import ChatIcon from '@mui/icons-material/Chat';
+import CustomerDrawer from './CustomerDrawer';
+import useDimensions from '@hooks/useDimensions';
+
 
 const Customer = ({ open, handleDrawerClose, textSelectorOpen, toggleTextSelector }) => {
   const [BalancePopupOpen, setBalancePopupOpen] = useState(false)
   const [openPaymentDailog, setOpenPaymentDailog] = useState(false);
   const [openCreateCustomerModel, setOpenCreateCustomerModel] = useState(false)
   const [settlementReportDialog, setSettlementReportDialog] = useState(false)
-  const [openLeftDrawerForMobileView, setOpenLeftDrawerForMobileView] = React.useState(false);
-  const isMobile = (window.innerWidth < 500);
+  const [openLeftDrawerForMobileView, setOpenLeftDrawerForMobileView] = useState(false);
+  const [responsiveDrawer, setResponsiveDrawer] = useState(false)
+  const [ref, dimensions] = useDimensions();
 
-
-  const handleClick = () => {
-    if (!isMobile) {
-      handleCloseForLeftDrawer();
-    }
-  };
-
-  const handleTouchMove = () => {
-    if (isMobile) {
-      handleCloseForLeftDrawer();
-    }
-  };
+  const handleResponsiveDrawer = () => {
+    setResponsiveDrawer(!responsiveDrawer)
+  }
 
   const handleClickForLeftDrawer = () => {
-    console.log(isMobile)
     setOpenLeftDrawerForMobileView(true);
   };
-
-  const handleCloseForLeftDrawer = () => {
-    setOpenLeftDrawerForMobileView(false);
-  };
-
 
   const handleClickOpen = () => {
     setOpenPaymentDailog(true);
@@ -88,7 +77,12 @@ const Customer = ({ open, handleDrawerClose, textSelectorOpen, toggleTextSelecto
       <ApplyPayment isOpen={openPaymentDailog} handleApplyPaymentDailogClose={handleApplyPaymentDailogClose} />
       <CreateCustomer isOpen={openCreateCustomerModel} handleClose={clickFroCreateCustomerModel} />
       <SettlementReportDialog isOpen={settlementReportDialog} handleSettlementReportDialog={handleSettlementReportDialog} />
-
+      <CustomerDrawer 
+         height={dimensions.height}
+         width={dimensions.width}
+         openResponsiveDrawer={responsiveDrawer}
+         closeResponsiveDrawer={handleResponsiveDrawer}
+      />
 
       <div className={styles.container}
         style={bodyStyles}
@@ -184,129 +178,9 @@ const Customer = ({ open, handleDrawerClose, textSelectorOpen, toggleTextSelecto
 
               </div>
             </div>
-            <div className={styles.contentWrapper}>
+            <div className={styles.contentWrapper} ref={ref}>
               <div className={styles.heading}>
-                <ChatIcon className={styles.openDailogIcon} onClick={handleClickForLeftDrawer} />
-                <Dialog
-                  sx={{
-                    position:'absolute',
-                    left:0,
-                    right:0,
-                    zIndex: 1,
-                    '& .css-1t1j96h-MuiPaper-root-MuiDialog-paper ': {
-                      height: '79%',
-
-                    },
-                    '& .css-ypiqx9-MuiDialogContent-root': {
-                      padding: '20px'
-                    },
-                    '& .css-yiavyu-MuiBackdrop-root-MuiDialog-backdrop': {
-                      backgroundColor: isMobile ? 'unset' : 'set',
-                    },
-                  }}
-                  onTouchMove={handleTouchMove}
-                  onClick={handleClick}
-                  open={openLeftDrawerForMobileView}
-                  aria-labelledby="alert-dialog-title"
-                  aria-describedby="alert-dialog-description"
-                >
-                  <DialogContent
-                  >
-                    <div className={styles.drawer}>
-                      <div className={styles.searchbar}>
-                        <TextField
-                          size="small"
-                          placeholder='Search Customers'
-                          variant="outlined"
-                          sx={{
-                            '& .css-1q6at85-MuiInputBase-root-MuiOutlinedInput-root': {
-                              borderRadius: '20px',
-                            },
-                            '& .css-1ua80n0-MuiInputBase-input-MuiOutlinedInput-input': {
-                              padding: '05px'
-                            },
-                            boxShadow: 'rgba(0, 0, 0, 0.24) 0px 1px 2px;',
-                            border: 'none',
-                            flexGrow: 1,
-                            width: '100%',
-                            borderRadius: '20px',
-                            '@media (max-width: 1200px)': {
-                              width: '100%',
-                              backgroundColor: 'white'
-                            }
-
-                          }}
-                          InputProps={{
-
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <SearchIcon sx={{ color: 'black' }} />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </div>
-                      <div className={styles.listoficon}>
-                        <Tooltip title="Reconcile Invoices" placement="top">
-                          <FactCheckIcon sx={{ cursor: 'pointer' }} />
-                        </Tooltip>
-                        <Tooltip title="Customer Balance & Aging Summary" placement="top">
-                          <MonetizationOnIcon sx={{ cursor: 'pointer' }} onClick={BalancePopupRendered} />
-                        </Tooltip>
-                        <Tooltip title="Apply Payments" placement="top">
-                          <PaymentsIcon sx={{ cursor: 'pointer' }} onClick={handleClickOpen} />
-                        </Tooltip>
-                        <Tooltip title="Download Pending Reconcile Report" placement="top">
-                          <FileDownloadIcon sx={{ cursor: 'pointer' }} />
-                        </Tooltip>
-                        <Tooltip title="Create Customer" placement="top">
-                          <AddCircleIcon sx={{ cursor: 'pointer' }} onClick={clickFroCreateCustomerModel} />
-                        </Tooltip>
-                        <Tooltip title="Credit & Bank Transfer Settlement Report" placement="top">
-                          <img
-                            src={credit}
-                            alt='credit'
-                            className={styles.credit}
-                            style={{ cursor: 'pointer' }}
-                            onClick={handleSettlementReportDialog}
-                          />
-                        </Tooltip>
-                        <Tooltip title="Archived Customers" placement="top">
-                          <ArchiveIcon sx={{ cursor: 'pointer' }} />
-                        </Tooltip>
-                        <Tooltip title="Global Invoice Search" placement="top">
-                          <TravelExploreIcon sx={{ cursor: 'pointer' }} />
-                        </Tooltip>
-                        <Tooltip title="Global Invoiced Ticket Search" placement="top">
-                          <ManageSearchIcon sx={{ cursor: 'pointer' }} />
-                        </Tooltip>
-                      </div>
-                      <div className={styles.profile}>
-                        <Avatar
-                          sx={{
-                            height: '40px',
-                            width: '40px',
-                            fontWeight: 800,
-                            fontSize: '16px',
-                            color: 'black'
-                          }}
-                          {...stringAvatar('Kent Dodds')}
-                        />
-                        <div className={styles.name}>
-                          kent dodds
-                          <div className={styles.balance}>
-                            <b>Balance</b>  $0.00
-                          </div>
-                        </div>
-
-                      </div>
-                    </div>
-
-                  </DialogContent>
-
-                </Dialog>
-
-
+                <ChatIcon className={styles.openDailogIcon} onClick={handleResponsiveDrawer} />
                 Customer Details
               </div>
               <div className={styles.box}>
