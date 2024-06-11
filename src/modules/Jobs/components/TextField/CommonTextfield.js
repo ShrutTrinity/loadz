27cdcc100e8f91466d2c05805f96a15a517b3264
ShrutTrinity <shrut.trinityinfosystem@gmail.com>
2024-06-11 17:24:57 +0530

    add a unit routes

--- src/modules/Jobs/components/TextField/CommonTextfield.js
+++ src/modules/Jobs/components/TextField/CommonTextfield.js
@@ -6,27 +6,27 @@ const CommonTextfield = (props) => {
     <>
       <TextField
         sx={{
-          
+          background: 'rgb(255,255,255)',
           width: `${props.width}%`,
           margin: '13px 0 13px 0',
-          '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input ':{
-            padding:'12.5px 14px',
-            height:`${props.height}`,
-            
+          '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input ': {
+            padding: '12.5px 14px',
+            height: `${props.height}`,
+
           },
           "& input::placeholder": {
             fontSize: "13px"
           },
-          
+
           '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
             borderColor: 'rgba(0, 0, 0, 0.23)',
           },
           '& .MuiInputLabel-outlined.Mui-focused': {
             color: 'rgb(237, 202, 51)',
           },
-          '& .css-14s5rfu-MuiFormLabel-root-MuiInputLabel-root':{
+          '& .css-14s5rfu-MuiFormLabel-root-MuiInputLabel-root': {
             fontSize: '14px',
-            height:`${props.height}`,
+            height: `${props.height}`,
           },
           '@media (max-width: 766px)': {
             width: '97%'
@@ -39,7 +39,7 @@ const CommonTextfield = (props) => {
         type={props.type}
         InputProps={{
           endAdornment: <InputAdornment position="start">{props.end}</InputAdornment>,
-          }}
+        }}
         label={props.label}
         value={props.value}
         onChange={props.onChange}
