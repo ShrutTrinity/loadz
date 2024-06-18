3d46dbeb9178ffd22b7d424c3def1b9fb3073506
ShrutTrinity <shrut.trinityinfosystem@gmail.com>
2024-06-18 17:59:11 +0530

    add dialog

--- src/modules/Jobs/components/BillingBody/AddUnitDialog.js
+++ src/modules/Jobs/components/BillingBody/AddUnitDialog.js
@@ -1,44 +1,74 @@
 import * as React from 'react';
+import styles from './styles/unitDialog.module.scss';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import CommonTextfield from '@Jobs/components/TextField/CommonTextfield';
+
+const allStyles = {
+  btns: {
+    color: 'rgb(237,202,51)',
+    textTransform: 'capitalize'
+  }
+}
 
 const AddUnitDialog = (props) => {
 
   const { handleClose, open } = props
 
-
-
   return (
-    <React.Fragment>
-
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      sx={{
+        '& .MuiDialog-paper': {
+          borderRadius: '18px'
+        }
+      }}
+    >
+      <DialogTitle id="alert-dialog-title">
+        <section className={styles.header}>
+          Add new Units
+        </section>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          <span className={styles.instruction}>Please fill this form with the information requested for create a Units</span>
+          <form className={styles.form}>
+            <CommonTextfield
+              id='unitName'
+              name='unitName'
+              type='text'
+              label='Unit Name'
+              width='100'
+              required
+            />
+            <CommonTextfield
+              id='unitMetric' 
+              name='unitMetric'
+              type='text'
+              label='Unit Metric'
+              width='100'
+              required
+            />
+          </form>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} style={allStyles.btns}>
+          Cancel
+        </Button>
+        <Button onClick={handleClose} style={allStyles.btns}>
+          Create
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
-export default AddUnitDialog
+export default AddUnitDialog;
